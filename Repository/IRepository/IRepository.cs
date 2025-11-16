0d0ed631a411a2f4551bd42d79f90b165cfdacd2
Tab Mutua <mukamimutua931@gmail.com>
2025-11-16 23:34:43 +0300

    IRepository

---
+++ Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace RideShareAPI.Repository.IRepository;
+
+public interface IRepository<T> where T:class
+{
+    Task <List<T>> GetAllAsync(Expression<Func<T,bool>>? filter = null,bool tracked=true,params Expression<Func<T, object>>[] includeProperties);
+    Task<T> GetAsync(Expression<Func<T, bool>> filter,bool tracked=true, params Expression<Func<T, object>>[] includeProperties);
+    Task CreateAsync(T entity);
+    Task RemoveAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task SaveAsync();
+    
+}
