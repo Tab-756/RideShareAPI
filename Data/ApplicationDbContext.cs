d62deda730fae7648d83ab04351ccd8db4ba45e2
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-11-19 22:27:15 +0300

    deletebehavior restrict

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,10 +12,13 @@ public class ApplicationDbContext:DbContext
     }
     public DbSet<User> Users { get; set; }
     public DbSet<Ride> Rides { get; set; }
+    public DbSet<RideRequest> RideRequests { get; set; }
     
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<User>().HasMany(u => u.Rides).WithOne(r => r.User).HasForeignKey(r => r.UserId)
+        modelBuilder.Entity<User>().HasMany(u => u.Rides).WithOne(r => r.User).HasForeignKey(r => r.DriverId)
             .OnDelete(DeleteBehavior.Cascade);
+        modelBuilder.Entity<User>().HasMany(u => u.RideRequests).WithOne(r => r.User).HasForeignKey(r => r.RiderId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
