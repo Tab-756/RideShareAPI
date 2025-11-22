7532c601da5c82b2ee6f0e96c2b8fca7cb1a295f
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-11-22 22:14:33 +0300

    RideRequestRepository  and RideRepository DI registration

--- Program.cs
+++ Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => {
 
 builder.Services.AddControllers();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IRideRepository, RideRepository>();
+builder.Services.AddScoped<IRideRequestRepository, RideRequestRepository>();
 
 
 var jwtSecretKey = builder.Configuration.GetValue<string>("ApiSettings:Secret");
