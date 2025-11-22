13a937f51e726ddc95c66c23568ff229557c79e8
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-11-23 02:46:36 +0300

    RideUpdateDTO.cs

---
+++ Models/DTO/RideUpdateDTO.cs
@@ -0,0 +1,13 @@
+namespace RideShareAPI.Models.DTO;
+
+public class RideUpdateDTO
+{
+    public int RideId { get; set; }
+    public string Source { get; set; }
+    public string Destination { get; set; }
+    public DateTime StartTime { get; set; }
+    public decimal PricePerSeat { get; set; }
+    public RideStatus Status { get; set; } = RideStatus.Scheduled;
+    public string RegistrationNumber { get; set; }
+    public string Description { get; set; }
+}
