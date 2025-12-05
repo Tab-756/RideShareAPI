5d75a62269b6f5e67304682b5ceb8a6c837e7422
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-12-05 13:33:42 +0300

    UserRepository.cs

--- Repository/UserRepository.cs
+++ Repository/UserRepository.cs
@@ -1,8 +1,12 @@
+using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using RideShareAPI.Data;
 using RideShareAPI.Models;
