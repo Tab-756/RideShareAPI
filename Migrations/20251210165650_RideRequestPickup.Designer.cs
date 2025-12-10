e65e70645b599eb8575fa379862c39147542d311
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-12-10 20:12:48 +0300

    RideRequest Db Improvements

--- Migrations/20251210165650_RideRequestPickup.Designer.cs
+++ Migrations/20251210165650_RideRequestPickup.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RideShareAPI.Data;
 
@@ -11,9 +12,11 @@ using RideShareAPI.Data;
 namespace RideShareAPI.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251210165650_RideRequestPickup")]
+    partial class RideRequestPickup
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
