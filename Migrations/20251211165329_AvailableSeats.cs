87afe3cc571c5696c96dea287d455787376264c4
Justus Kyalo <jkyalo@seaburysolutions.com>
2025-12-11 19:54:49 +0300

    Available seats Column

---
+++ Migrations/20251211165329_AvailableSeats.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RideShareAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AvailableSeats : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AvailableSeats",
+                table: "Rides",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvailableSeats",
+                table: "Rides");
+        }
+    }
+}
